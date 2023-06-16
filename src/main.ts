3be92be89d8bebeba943c6c3182bc07ba4b9dd03
pollyjean <pollyjean@naver.com>
2023-06-16 16:48:23 +0900

    remove not use imgs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import './index.css'
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
